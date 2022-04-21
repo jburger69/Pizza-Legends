4adef8ea44bcab55e0c37ad53e45276793feff13
Joshua Burger <joshberger6969@outlook.com>
2022-04-21 16:23:28 -0400

    Create shadow for sprite when initialized

--- Sprite.js
+++ Sprite.js
@@ -8,6 +8,16 @@ class Sprite {
             this.isLoaded = true;
         }
 
+        //Shadow
+        this.shadow = new Image();
+        this.useShadow = true; //config.useShadow || false
+        if (this.useShadow) {
+            this.shadow.scr = "/images/characters/shadow.png";
+        }
+        this.shadow.onload = () => {
+            this.isShadowLoaded = true;
+        }
+
 
         // Configure Animation & Initial State
         this.animations = config.animations || {
@@ -26,11 +36,13 @@ class Sprite {
         const x = this.gameObject.x * 16 - 8;
         const y = this.gameObject.y * 16 - 18;
 
+        this.isShadowLoaded && ctx.drawImage(this.shadow, x, y);
+
         this.isLoaded && ctx.drawImage(this.image,
             0,0,
             32,32,
             x,y,
             32,32
-        )
+        );
     }
 }
