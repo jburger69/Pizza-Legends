4ebd9e35b0eacf784c2da0ccb65e9254a888ba76
Joshua Burger <joshberger6969@outlook.com>
2022-04-21 21:37:32 -0400

    Remove uneeded code, Create game loop

--- Overworld.js
+++ Overworld.js
@@ -3,10 +3,22 @@ class Overworld {
         this.element = config.element;
         this.canvas = this.element.querySelector(".game-canvas");
         this.ctx = this.canvas.getContext("2d");
+        this.map = null;
     }
 
     startGameLoop() {
         const step = () => {
+            //Draw Lower layer
+            this.map.drawLowerImage(this.ctx);
+
+            // Draw Game Objects
+            Object.values(this.map.gameObjects).forEach(object => {
+                object.sprite.draw(this.ctx);
+            })
+
+            //Draw Upper layer
+            this.map.drawUpperImage(this.ctx);
+
             requestAnimationFrame(() => {
                 step();
             })
@@ -15,28 +27,9 @@ class Overworld {
     }
 
     init() {
+        this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
         this.startGameLoop();
 
-        const image = new Image();
-        image.onload = () => {
-          this.ctx.drawImage(image,0,0)
-        };
-        image.src = "images/maps/DemoLower.png";
-     
-        // Place some Game Objects!
-        const hero = new GameObject({
-            x: 5,
-            y: 6,
-        })
-        const npc1 = new GameObject({
-            x: 7,
-            y: 9,
-            src: "/images/characters/people/npc1.png"
-        })
-
-        setTimeout(() => {
-            hero.sprite.draw(this.ctx);
-            npc1.sprite.draw(this.ctx);
-        }, 200)
+        
     }
 }
